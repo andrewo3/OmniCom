f1770c1b81233dbc1a6313edb09de8a92cfd4463
Fencerr <43830774+Fencerr@users.noreply.github.com>
2024-01-05 20:49:21 -0500

    fixed linux script

--- bin/linux_build_script.sh
+++ bin/linux_build_script.sh
@@ -1,3 +1,3 @@
 xxd -n fragment -i res/shaders/main.frag > include/universal/shader_data.h
 xxd -n vertex -i res/shaders/main.vert >> include/universal/shader_data.h
-g++ src/util.cpp src/rom.cpp src/cpu.cpp src/cpu_helper.cpp src/ppu.cpp src/apu.cpp -g src/main.cpp -Iinclude/universal -lSDL2 -lGL -lGLEW -lGLU -o "/WIN_D/Projects/Visual Studio Code/Nes2Execv2/bin/main"
+g++ src/ntsc-filter/crt_core.c src/util.cpp src/rom.cpp src/cpu.cpp src/cpu_helper.cpp src/ppu.cpp src/apu.cpp -g src/main.cpp -Iinclude/universal -Isrc/ntsc-filter -lSDL2 -lGL -lGLEW -lGLU -o "/WIN_D/Projects/Visual Studio Code/Nes2Execv2/bin/main"
