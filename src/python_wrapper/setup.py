094aeac067b90d15b748fbc43c4fff00bfff3a7f
Andrew Ogundimu <andrew.ogundimu@gmail.com>
2024-02-28 12:33:26 -0500

    OS dependent env vars in setuptools

--- src/python_wrapper/setup.py
+++ src/python_wrapper/setup.py
@@ -1,21 +1,37 @@
 from glob import glob
-from os import environ
+from sys import platform
+from os import environ, chdir
 from os.path import *
 from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension
 
+cwd = dirname(realpath(__file__))
+chdir(cwd)
 #sorted(glob("*.cpp")),  # Sort source files for reproducibility
+files = sorted(glob("../*.cpp"))
+files.append("wrapper.cpp")
+files.remove("../main.cpp")
+libs = sorted(glob("../../lib/*.*"))
 ext_modules = [
     Pybind11Extension(
         "pyNES",
-        ["wrapper.cpp"]
+        files,
+        
     ),
 ]
+sep = ":"
+if platform == "win32":
+    sep = ";"
 
-include_path = f"{abspath("..\\..\\include\\unix")};{abspath("..\\..\\include\\win32")};{abspath("..\\ntsc-filter")};{abspath("..\\..\\include\\universal")};{abspath("..\\imgui")};{abspath("..\\imgui\\backends")}"
+include_path = f"{realpath("../../include/unix")}{sep}{realpath("../../include/win32")}{sep}{realpath("../ntsc-filter")}{sep}{realpath("../../include/universal")}{sep}{realpath("../imgui")}{sep}{realpath("../imgui/backends")}"
 
-environ["INCLUDE"] = include_path
-environ["LIB"] = abspath("..\\..\\lib")
+if platform == "win32":
+    environ["INCLUDE"] = include_path
+    environ["LIB"] = realpath("../../lib")
+else:
+    environ["CFLAGS"]="-std=c++11"
+    environ["CPLUS_INCLUDE_PATH"] += include_path
+    environ["LD_LIBRARY_PATH"] = realpath("../../lib")
 
 setup(
     name='pyNES',
