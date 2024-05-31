7580881a15011083137ff42aa4b98d1fa934555e
Andrew Ogundimu <andrew.ogundimu@gmail.com>
2024-05-31 01:20:45 -0400

    python version 0.3.1

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ ext_modules = [
 
 setup(
     name='omnicom',
-    version='0.3.0',
+    version='0.3.1',
     author="Andrew Ogundimu",
     description="A python module for a multisystem (currently only NES) emulator",
     ext_modules=ext_modules,
