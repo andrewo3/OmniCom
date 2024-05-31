20ca76587e461ccdf417516f4e2a603038f63cfb
Andrew Ogundimu <andrew.ogundimu@gmail.com>
2024-05-30 22:43:54 -0400

    proxy setup.py edits

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ import os
 import sys
 
 print(sys.argv)
-command = f"python src/NES/python_wrapper/{" ".join(sys.argv)}"
+command = "python src/NES/python_wrapper/"+(" ".join(sys.argv))
 print(command)
 os.system(command)
